ec5a03e1cdaaf48a14ae5eaa862019f83e464230
Charles Madere <charlesrmadere@gmail.com>
2025-03-16 03:45:34 +0900

    small python updates

--- extron.py
+++ extron.py
@@ -10,11 +10,13 @@ BAUD_RATE = 9600
 # Replace "COM3" with the appropriate COM port
 COM_PORT = "COM4"
 
-def sendCommand(command: str):
+def setExtronToPreset(presetNumber: int):
+    command = f'{presetNumber}.'
+
     """Send a command to the Extron MVX 84 VGA A."""
     try:
         with serial.Serial(COM_PORT, BAUD_RATE, timeout = 1) as ser:
-            ser.write(command.encode('utf-8') + b'\r') # Append carriage return
+            ser.write(command.encode("utf-8") + b'\r') # Append carriage return
             time.sleep(0.2) # Wait for the device to process
             response = ser.read(100) # Read up to 100 bytes
             print("Response:", response.decode('utf-8', errors = 'ignore'))
@@ -25,18 +27,17 @@ if __name__ == "__main__":
     arguments: list[str] | Any | None = sys.argv
     presetNumber: int
 
-    if isinstance(arguments, list) and len(arguments) >= 2:
-        presetNumberArgument: str | Any | None = arguments[1]
+    if not isinstance(arguments, list) or len(arguments) < 2:
+        raise RuntimeError(f"Preset number argument was not set ({arguments=})")
 
-        try:
-            presetNumber = int(presetNumberArgument)
-        except Exception as e:
-            raise RuntimeError(f"Preset number parse error ({presetNumberArgument=}) ({arguments=}): {e}")
+    presetNumberArgument: str | Any | None = arguments[1]
 
-        if presetNumber < 0 or presetNumber > 8:
-            raise RuntimeError(f"Preset number is out of bounds ({presetNumber=}) ({presetNumberArgument=}) ({arguments=})")
-    else:
-        raise RuntimeError(f"No preset number argument specified ({arguments=})")
+    try:
+        presetNumber = int(presetNumberArgument)
+    except Exception as e:
+        raise RuntimeError(f"Preset number parse error ({presetNumberArgument=}) ({arguments=}): {e}")
 
-    fullCommand = f"{presetNumber}."
-    sendCommand(fullCommand)
+    if presetNumber < 0 or presetNumber > 8:
+        raise RuntimeError(f"Preset number is out of bounds ({presetNumber=}) ({presetNumberArgument=}) ({arguments=})")
+
+    setExtronToPreset(presetNumber)
