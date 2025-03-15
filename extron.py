91e0fb794332a149783723123f8c35622736514b
Charles Madere <charlesrmadere@gmail.com>
2025-03-16 04:09:31 +0900

    determine input configuration based on a string

--- extron.py
+++ extron.py
@@ -1,22 +1,85 @@
-import serial
+import re
 import sys
 import time
+from enum import Enum, auto
 from typing import Any
 
+import serial
 
 # the Extron MVX 84 VGA uses a baud rate of 9600
 BAUD_RATE = 9600
 
-# Replace "COM3" with the appropriate COM port
+# the Extron MVX 84 VGA is currently using COM4 on my system
 COM_PORT = "COM4"
 
-def setExtronToPreset(presetNumber: int):
-    command = f'{presetNumber}.'
+class ConsoleConfiguration(Enum):
+    GAME_CUBE = auto()
+    MEGA_SG = auto()
+    NINTENDO_64 = auto()
+    NT_MINI_NOIR = auto()
+    SONY_PS1 = auto()
+    SONY_PS3 = auto()
+    SUPER_FAMICOM = auto()
 
-    """Send a command to the Extron MVX 84 VGA A."""
+    @property
+    def extronPreset(self) -> int:
+        match self:
+            case ConsoleConfiguration.GAME_CUBE: return 6
+            case ConsoleConfiguration.MEGA_SG: return 2
+            case ConsoleConfiguration.NINTENDO_64: return 5
+            case ConsoleConfiguration.NT_MINI_NOIR: return 1
+            case ConsoleConfiguration.SONY_PS1: return 4
+            case ConsoleConfiguration.SONY_PS3: return 7
+            case ConsoleConfiguration.SUPER_FAMICOM: return 3
+            case _: raise ValueError(f'Unknown ConsoleConfiguration value: \"{self}\"')
+
+    @property
+    def hdmiPort(self) -> int:
+        match self:
+            case ConsoleConfiguration.GAME_CUBE: return 5
+            case ConsoleConfiguration.MEGA_SG: return 1
+            case ConsoleConfiguration.NINTENDO_64: return 4
+            case ConsoleConfiguration.NT_MINI_NOIR: return 2
+            case ConsoleConfiguration.SONY_PS1: return 3
+            case ConsoleConfiguration.SONY_PS3: return 1
+            case ConsoleConfiguration.SUPER_FAMICOM: return 1
+            case _: raise ValueError(f'Unknown ConsoleConfiguration value: \"{self}\"')
+
+def determineConsoleConfiguration(consoleArgument: str) -> ConsoleConfiguration:
+    if re.fullmatch(r'^\s*game(?:\s+|_|-)?cube\s*$', consoleArgument, re.IGNORECASE):
+        return ConsoleConfiguration.GAME_CUBE
+
+    elif re.fullmatch(r'^\s*mega(?:\s+|_|-)?sg\s*$', consoleArgument, re.IGNORECASE):
+        return ConsoleConfiguration.MEGA_SG
+
+    elif re.fullmatch(r'^\s*nintendo(?:\s+|_|-)?64\s*$', consoleArgument, re.IGNORECASE):
+        return ConsoleConfiguration.NINTENDO_64
+
+    elif re.fullmatch(r'^\s*nt(?:\s+|_|-)?mini(?:(?:\s+|_|-)noir)?\s*$', consoleArgument, re.IGNORECASE):
+        return ConsoleConfiguration.NT_MINI_NOIR
+
+    elif re.fullmatch(r'^\s*(?:sony(?:\s+|_|-)?)?ps1\s*$', consoleArgument, re.IGNORECASE):
+        return ConsoleConfiguration.SONY_PS1
+
+    elif re.fullmatch(r'^\s*(?:sony(?:\s+|_|-)?)?ps3\s*$', consoleArgument, re.IGNORECASE):
+        return ConsoleConfiguration.SONY_PS3
+
+    elif re.fullmatch(r'^\s*super(?:\s+|_|-)?famicom\s*$', consoleArgument, re.IGNORECASE):
+        return ConsoleConfiguration.SUPER_FAMICOM
+
+    else:
+        raise ValueError(f'The given consoleArgument value has no matching ConsoleConfiguration: \"{consoleArgument}\"')
+
+def setHdmiMatrixConfiguration(configuration: ConsoleConfiguration):
+    # TODO
+    pass
+
+def setVgaMatrixConfiguration(configuration: ConsoleConfiguration):
+    extronCommand = f'{configuration.extronPreset}.'
+
     try:
         with serial.Serial(COM_PORT, BAUD_RATE, timeout = 1) as ser:
-            ser.write(command.encode("utf-8") + b'\r') # Append carriage return
+            ser.write(extronCommand.encode("utf-8") + b'\r') # Append carriage return
             time.sleep(0.2) # Wait for the device to process
             response = ser.read(100) # Read up to 100 bytes
             print("Response:", response.decode('utf-8', errors = 'ignore'))
@@ -30,14 +93,10 @@ if __name__ == "__main__":
     if not isinstance(arguments, list) or len(arguments) < 2:
         raise RuntimeError(f"Preset number argument was not set ({arguments=})")
 
-    presetNumberArgument: str | Any | None = arguments[1]
+    consoleArgument: str | Any | None = arguments[1]
 
-    try:
-        presetNumber = int(presetNumberArgument)
-    except Exception as e:
-        raise RuntimeError(f"Preset number parse error ({presetNumberArgument=}) ({arguments=}): {e}")
+    if not isinstance(consoleArgument, str) or len(consoleArgument) == 0 or consoleArgument.isspace():
+        raise RuntimeError(f"Console configuration argument is malformed ({consoleArgument=}) ({arguments=})")
 
-    if presetNumber < 0 or presetNumber > 8:
-        raise RuntimeError(f"Preset number is out of bounds ({presetNumber=}) ({presetNumberArgument=}) ({arguments=})")
-
-    setExtronToPreset(presetNumber)
+    consoleConfiguration = determineConsoleConfiguration(consoleArgument)
+    setVgaMatrixConfiguration(consoleConfiguration)
