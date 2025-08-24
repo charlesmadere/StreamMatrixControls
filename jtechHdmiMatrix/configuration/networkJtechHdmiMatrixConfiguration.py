a5aed5b15bcee78669cacb2885dfd286b5fcccbf
Charles Madere <charlesrmadere@gmail.com>
2025-08-24 15:07:19 +0900

    tiny python cleanup

--- jtechHdmiMatrix/configuration/networkJtechHdmiMatrixConfiguration.py
+++ jtechHdmiMatrix/configuration/networkJtechHdmiMatrixConfiguration.py
@@ -6,4 +6,4 @@ from .absJtechHdmiMatrixConfiguration import AbsJtechHdmiMatrixConfiguration
 @dataclass(frozen = True)
 class NetworkJtechHdmiMatrixConfiguration(AbsJtechHdmiMatrixConfiguration):
     portCount: int = 8
-    ipAddress: str = "http://192.168.1.7"
+    ipAddress: str = 'http://192.168.1.7'
